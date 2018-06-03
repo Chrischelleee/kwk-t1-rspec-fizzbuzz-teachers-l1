826a5196941995574185d9549ec9744aa9b761d5
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-03 17:13:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,25 @@
+# def fizzbuzz(int)
+#   if int % 3 == 0
+#     "Fizz"
+#   elsif int % 5 == 0
+#     "Buzz"
+#   elsif int % 3 and 5 == 0 
+#     "FizzBuzz"
+#   else int % 4 != 0
+#     nil
+#   end
+# end
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-  elsif int % 5 == 0
-    "Buzz"
-  elsif int % 3 and 5 == 0 
-    "FizzBuzz"
-  else int % 4 != 0
-    nil
+  if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
   end
+
+  if int % 5 == 0
+   return "Buzz"
+  end
+
+  if int % 3 == 0
+   return "Fizz"
+  end
+
 end
