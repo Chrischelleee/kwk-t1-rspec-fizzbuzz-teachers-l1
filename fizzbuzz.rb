05f9ae2ad19607f81556f48aed0f1a193a555222
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-03 16:53:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-elsif int % 5 == 0
+  elsif int % 5 == 0
   "Buzz"
-elsif int % 15 == 0
+  elsif int % 15 == 0
   "FizzBuzz"
-else int % 4 != 0
+  else int % 4 != 0
   nil
   end
 end
